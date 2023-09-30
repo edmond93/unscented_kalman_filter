9150de5a67a8d9dfab505ed6013a4f576ee3361c
Edmond Ang <edmond@otsaw.com>
2023-09-30 11:29:04 +0800

    Added docker setup for ws (x86_64) only for now

---
+++ docker/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+DEFAULT_USER_ID=1000
+
+#
+# Ensure host and container have the same user ID. This is to allow both sides
+# to read and write the shared directories.
+#
+if [ -v USER_ID ] && [ "$USER_ID" != "$DEFAULT_USER_ID" ]; then
+    echo "Changing robot user ID to match your host's user ID ($USER_ID)."
+    echo "This operation can take a while..."
+
+    usermod --uid $USER_ID robot
+
+    # Ensure all files in the home directory are owned by the new user ID
+    find /home/robot -user $DEFAULT_USER_ID -exec chown -h $USER_ID {} \;
+fi
+
+cd /home/robot
+
+# If no command is provided, set bash to start interactive shell
+if [ -z "$1" ]; then
+    set - "/bin/bash" -l
+fi
+
+# Run the provided command using user 'robot'
+exec "$@"
